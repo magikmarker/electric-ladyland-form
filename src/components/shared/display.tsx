b77b3adb975c8e7f7c8ec517a9174ed7db7d2d33
markingvar <office@jensendesignshop.com>
2022-08-08 20:43:15 -0700

    add click targets to labels

--- src/components/shared/display.tsx
+++ src/components/shared/display.tsx
@@ -113,7 +113,7 @@ export function RadioOrCheckboxWrapper({
   return (
     <>
       <div
-        className={`el-radio-or-checkbox-wrapper${
+        className={`el-radio-or-checkbox-wrapper expand-click-target${
           className ? " " + className : ""
         }`}
       >
@@ -135,7 +135,7 @@ export function RadioOrCheckboxLabel({
   return (
     <label
       htmlFor={htmlFor}
-      className={`el-radio-or-checkbox-label${
+      className={`el-radio-or-checkbox-label expand-click-target${
         className ? " " + className : ""
       }`}
     >
